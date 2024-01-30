7a49273ca10fd1b60d1c627807f76c38a785d8bd
김수환 <101265039+genius00hwan@users.noreply.github.com>
2024-01-30 17:49:50 +0900

    feat : create responseDTO for class side note

---
+++ src/main/java/UMC/campusNote/classSideNote/dto/ClassSideNoteResponse.java
@@ -0,0 +1,47 @@
+package UMC.campusNote.classSideNote.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ClassSideNoteResponse {
+
+    @NonNull
+    private Long id;
+
+    @NonNull
+    private String content;
+
+    @NonNull
+    private Boolean isTodo;
+
+    @NonNull
+    private Integer colorCode;
+
+    private String deadline;
+
+    private Integer errorFlag;
+
+
+    public ClassSideNoteResponse(@NonNull Long id, @NonNull String content, @NonNull Boolean isTodo,
+                                 @NonNull Integer colorCode, String deadline) {
+        this.id = id;
+        this.content = content;
+        this.isTodo = isTodo;
+        this.colorCode = colorCode;
+        this.deadline = deadline;
+    }
+
+    public ClassSideNoteResponse(Integer errorFlag) {
+        this.errorFlag = errorFlag;
+    }
+
+//    private LocalDateTime createdAt;
+//
+//    private LocalDateTime updatedAt;
+}
