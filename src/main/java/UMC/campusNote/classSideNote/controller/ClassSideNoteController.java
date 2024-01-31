cce26f988797cbc2338cbd965220231ad8e1e875
김수환 <101265039+genius00hwan@users.noreply.github.com>
2024-01-31 15:06:38 +0900

    refactor : 주석을 지우다

--- src/main/java/UMC/campusNote/classSideNote/controller/ClassSideNoteController.java
+++ src/main/java/UMC/campusNote/classSideNote/controller/ClassSideNoteController.java
@@ -3,11 +3,9 @@ package UMC.campusNote.classSideNote.controller;
 import UMC.campusNote.classSideNote.dto.ClassSideNoteRequest;
 import UMC.campusNote.classSideNote.dto.ClassSideNoteResponse;
 import UMC.campusNote.classSideNote.entity.ClassSideNote;
-import UMC.campusNote.classSideNote.exception.ClassSideNoteException;
 import UMC.campusNote.classSideNote.service.ClassSideNoteService;
 
 import UMC.campusNote.common.ApiResponse;
-
 
 import lombok.AllArgsConstructor;
 
@@ -30,70 +28,55 @@ public class ClassSideNoteController {
     ClassSideNoteService classSideNoteService;
 
     @PostMapping("/create/")
-    public ApiResponse<ClassSideNoteResponse> createClassSideNote(@RequestParam Long userLessonId,
-                                                                  @RequestBody ClassSideNoteRequest request) {
-        // Todo: Error 처리 로직 추가하기
-
-        //try {
-
+    public ApiResponse<ClassSideNoteResponse> createClassSideNote(
+            @RequestParam Long userLessonId,
+            @RequestBody ClassSideNoteRequest request) {
         ClassSideNote classSideNote = classSideNoteService.createClassSideNote(userLessonId, request);
 
         ClassSideNoteResponse response = convert(classSideNote);
 
         return ApiResponse.onSuccess(response);
-
-        //} catch (Exception e) {
-        //}
     }
 
 
     @GetMapping("/{id}")
-    public ApiResponse<ClassSideNoteResponse> getClassSideNoteById(@PathVariable Long id) {
-        // Todo: Error 처리 로직 추가하기
-//        try {
+    public ApiResponse<ClassSideNoteResponse> getClassSideNoteById(
+            @PathVariable Long id) {
         ClassSideNote classSideNote = classSideNoteService.getClassSideNoteById(id);
 
         ClassSideNoteResponse response = convert(classSideNote);
 
         return ApiResponse.onSuccess(response);
 
-//        } catch (ClassSideNoteException e) {
-//        }
     }
 
     @PutMapping("/{id}")
     public ApiResponse<ClassSideNoteResponse> updateClassSideNote(
-            @PathVariable Long id, @RequestBody ClassSideNoteRequest request) {
-        // Todo: Error 처리 로직 추가하기
-        //try {
+            @PathVariable Long id,
+            @RequestBody ClassSideNoteRequest request) {
         ClassSideNote classSideNote = classSideNoteService.updateClassSideNote(id, request);
 
         ClassSideNoteResponse response = convert(classSideNote);
 
         return ApiResponse.onSuccess(response);
-        //} catch (Exception e) {
-        //}
     }
 
     @PatchMapping("/{id}")
     public ApiResponse<ClassSideNoteResponse> updateClassSideNoteContent(
-            @PathVariable Long id, @RequestParam String content) {
-        // Todo: Error 처리 로직 추가하기
-        //try {
+            @PathVariable Long id,
+            @RequestParam String content) {
         ClassSideNote classSideNote = classSideNoteService.getClassSideNoteById(id);
         classSideNote.updateContent(content);
 
         ClassSideNoteResponse response = convert(classSideNote);
 
         return ApiResponse.onSuccess(response);
-        //} catch (Exception e) {
-        //}
     }
 
     @DeleteMapping("/{id}")
-    public ApiResponse<ClassSideNoteResponse> deleteClassSideNoteById(@PathVariable Long id) {
-        // Todo: Error 처리 로직 추가하기
-        //try {
+    public ApiResponse<ClassSideNoteResponse> deleteClassSideNoteById(
+            @PathVariable
+            Long id) {
         ClassSideNote classSideNote = classSideNoteService.getClassSideNoteById(id);
 
         classSideNoteService.deleteById(id);
@@ -102,8 +85,7 @@ public class ClassSideNoteController {
 
         return ApiResponse.onSuccess(response); // 삭제된 내용 반환
 
-        //} catch (Exception e) {
-        //}
+
     }
 
 
@@ -113,7 +95,8 @@ public class ClassSideNoteController {
                 .content(classSideNote.getContent())
                 .isTodo(classSideNote.getIsTodo())
                 .colorCode(classSideNote.getColorCode())
-                .deadline(classSideNote.getDeadline() != null ? classSideNote.getDeadline().toString() : null)
+                .deadline(classSideNote.getDeadline() != null ?
+                        classSideNote.getDeadline().toString() : null)
                 .build();
     }
 }
