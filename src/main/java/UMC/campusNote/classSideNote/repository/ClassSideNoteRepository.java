348a744247251aae8bf15e799537423ef81c0254
김수환 <101265039+genius00hwan@users.noreply.github.com>
2024-02-02 19:07:04 +0900

    refactor : UserLessonId 를 이용해 탐색하는 방법으로 수정

--- src/main/java/UMC/campusNote/classSideNote/repository/ClassSideNoteRepository.java
+++ src/main/java/UMC/campusNote/classSideNote/repository/ClassSideNoteRepository.java
@@ -1,16 +1,11 @@
 package UMC.campusNote.classSideNote.repository;
 
 import UMC.campusNote.classSideNote.entity.ClassSideNote;
-import UMC.campusNote.mapping.UserLesson.UserLesson;
 
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ClassSideNoteRepository extends JpaRepository<ClassSideNote, Long> {
-    List<ClassSideNote> findByUserLesson(UserLesson userLesson);
-    Slice<ClassSideNote> findSliceBy(UserLesson userLesson, Pageable pageable);
+    List<ClassSideNote> findByUserLessonId(Long userLessonId);
 }
