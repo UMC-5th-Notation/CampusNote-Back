27afa601eec207490da4cd24a91e40efea25cfef
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-02-15 17:22:59 +0900

    feat: 유저의 현재 학기 수정 API 스웨거 명세 추가 (#98)

--- src/main/java/UMC/campusNote/user/controller/UserController.java
+++ src/main/java/UMC/campusNote/user/controller/UserController.java
@@ -5,6 +5,8 @@ import UMC.campusNote.user.dto.UserRequestDTO;
 import UMC.campusNote.user.dto.UserResponseDTO;
 import UMC.campusNote.user.entity.User;
 import UMC.campusNote.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +26,12 @@ public class UserController {
         return ApiResponse.onSuccess("GET:: user controller");
     }
 
+
     @PatchMapping("/attended-semester")
+    @Operation(summary = "유저의 수강 학기 수정 API",description = "유저의 수강 학기 수정 API입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "ATTENDED_SEMESTER200",description = "수강 학기 업데이트 성공"),
+    })
     public ApiResponse<UserResponseDTO.AttendedSemesterUpdateDTO> updateAttendedSemester(@AuthenticationPrincipal User user, @RequestBody UserRequestDTO.AttendedSemesterUpdateDTO request) {
         return ApiResponse.of(ATTENDED_SEMESTER_UPDATE ,userService.updateAttendedSemester(user.getId(), request));
     }
